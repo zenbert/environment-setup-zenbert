05137f6f0083cdd2be0b3a3d281813b5cdf1e49b
Bert <alberwilly21@gmail.com>
2022-04-20 20:06:36 +0300

    Now it outputs the final position as it should.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,9 @@ public class Main {
         double fallingTime = 10.0;
         double initialPosition = 0.0;
         double finalPosition = 0.0;
+
+        finalPosition = 0.5 * gravity * (fallingTime * fallingTime) + initialVelocity * (fallingTime) + initialPosition;  
+
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
     }
